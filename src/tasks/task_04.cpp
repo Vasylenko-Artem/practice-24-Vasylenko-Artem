a877662f08de5f37ea7c2361c9e299f9d856dcc7
artemka <artem.vas333@gmail.com>
2025-06-22 16:15:24 +0300

    complete task_04

--- src/tasks/task_04.cpp
+++ src/tasks/task_04.cpp
@@ -1,3 +1,37 @@
 #include "tasks.h"
 
-void task_04() { std::cout << "Task 4 running\n"; }
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+void task_04()
+{
+	cout << "Enter at least two real numbers:\n";
+	istream_iterator<double> in(cin), end;
+	vector<double> numbers(in, end);
+
+	if (numbers.size() < 2)
+	{
+		cerr << "Error: Need at least two elements.\n";
+		// return 1;
+	}
+
+	vector<double> even_indexed;
+	int index = 0;
+
+	remove_copy_if(
+		numbers.begin(),
+		numbers.end(),
+		back_inserter(even_indexed),
+		[&index](double)
+		{ return ++index % 2 != 0; });
+
+	cout << "Elements with even positions:\n";
+	ostream_iterator<double> out(cout, " ");
+	copy(even_indexed.begin(), even_indexed.end(), out);
+
+	// return 0;
+}
